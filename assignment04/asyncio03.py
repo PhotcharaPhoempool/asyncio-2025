2d2faf1413b79e749576a534348232826d0751b0
PhotcharaPhoempool <6610301008@cdti.ac.th>
2025-06-25 10:54:48 +0700

    assignment04 asyncio03

--- assignment04/asyncio03.py
+++ assignment04/asyncio03.py
@@ -1,2 +1,18 @@
 # Create 2 Tasks with High-Level API
+import asyncio
 
+async def download_image(name, delay):
+    print(f"{name} downloading...")
+    await asyncio.sleep(delay)
+    print(f"{name} download complete!")
+
+async def main():
+    # create 2 tasks
+    task1 = asyncio.create_task(download_image("img 1", 2))
+    task2 = asyncio.create_task(download_image("img 2", 3))
+
+    # wait for both task finish
+    await task1
+    await task2
+
+asyncio.run(main())
