3f491a95cd638006777bf15199d5aeba2b4900e2
PhotcharaPhoempool <6610301008@cdti.ac.th>
2025-06-25 12:17:28 +0700

    breakfast03

--- assignment04/breakfast03.py
+++ assignment04/breakfast03.py
@@ -1,3 +1,28 @@
 # Asynchronous breakfast
 import asyncio
 from time import sleep, time
+
+async def make_coffee(start):  # 1
+    print(f"coffee: prepare ingridients {time()-start:.2f}")
+    sleep(1)
+    print("coffee: waiting...")
+    await asyncio.sleep(5)  # 2: pause, another tasks can be run
+    print("coffee: ready")
+
+async def fry_eggs(start):  # 1
+    print(f"eggs: prepare ingridients {time()-start:.2f}")
+    sleep(1)
+    print("eggs: frying...")
+    await asyncio.sleep(3)  # 2: pause, another tasks can be run
+    print("eggs: ready")
+
+async def main():  # 1
+    start = time()
+    coffee = asyncio.create_task(make_coffee(start))
+    egg = asyncio.create_task(fry_eggs(start))
+    await coffee  # run task with await
+    await egg
+    print(f"breakfast is ready in {time()-start:.2f} secs.")
+
+
+asyncio.run(main())  # run top-level function concurrently
