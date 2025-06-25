4ba7c0a630d94d496ef9aa0e79137e5e08ffaf9e
PhotcharaPhoempool <6610301008@cdti.ac.th>
2025-06-25 10:33:11 +0700

    assignment04 asyncio01

--- assignment04/asyncio01.py
+++ assignment04/asyncio01.py
@@ -1,1 +1,15 @@
 # example of getting the current task from the main coroutine
+# Check if a Task is Done
+import asyncio
+
+async def simple_task():
+    await asyncio.sleep(1)
+    return "เสร็จแล้ว!"
+
+async def  main():
+    task = asyncio.create_task(simple_task())
+    print("ก่อน await", task.done())
+    await task 
+    print("หลัง await", task.done())
+
+asyncio.run(main())
