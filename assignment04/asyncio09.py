8755578b0b2782368f70a8be74a8bf68a107264a
PhotcharaPhoempool <6610301008@cdti.ac.th>
2025-06-25 11:35:51 +0700

    assignment04 asyncio09

--- assignment04/asyncio09.py
+++ assignment04/asyncio09.py
@@ -1,1 +1,17 @@
 # cancel task
+import asyncio
+
+async def slow_task():
+    await asyncio.sleep(3)
+
+async def main():
+    task = asyncio.create_task(slow_task())
+    print("Cancel task in 1 sec")
+    await asyncio.sleep(1)
+    task.cancel()
+    try:
+        await task
+    except asyncio.CancelledError:
+        print("Task is cancel")
+
+asyncio.run(main())
