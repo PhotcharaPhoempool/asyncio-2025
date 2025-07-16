ba31d6e017d9c9219ba53711586e46b4a993e313
PhotcharaPhoempool <6610301008@cdti.ac.th>
2025-07-16 10:17:13 +0700

    assignment8 httpx1,2 pokemon1,2 sync1,2

---
+++ assignment08/httpx/pokemon01.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+response = requests.get(url)
+
+data = response.json()
+
+print("Name:", data["name"])
+print("ID:", data["id"])
+print("Height:", data["height"])
+print("Weight:", data["weight"])
+
+print("Type:", [t["type"]["name"] for t in data["types"]])
